896b0b48d5f6034a1448ddd3596f8fdf5bd929d0
Abdul Baseer <abdulbaseer.s130@gmail.com>
2025-01-10 22:11:54 +0500

    ok

--- src/pages/Signinsignup/Signinsignup.jsx
+++ src/pages/Signinsignup/Signinsignup.jsx
@@ -38,7 +38,7 @@ const Signinsignup = () => {
             // Sending a POST request to the backend API for login
             console.log("username : " + username + "\n" + "password : " + password);
 
-            const response = await axios.post('https://backend-8srpi5pxg-abdulbaseer-1s-projects.vercel.app/api/users/login',{
+            const response = await axios.post('backend-two-henna-56.vercel.app/api/users/login',{
                 username,
                 password, },
                 {
